eeae8f9761d70c353458c04224b745b64a076bd0
Canele-Lemon <gguguggaga@gmail.com>
2025-10-23 13:34:54 +0900

    VAC_optimization.py 업데이트

--- VAC_optimization.py
+++ VAC_optimization.py
@@ -1,16 +1,8 @@
-# (a) 예측 ON (W)
-lut256_iter = {
-    "R_Low": lut256["R_Low"], "G_Low": lut256["G_Low"], "B_Low": lut256["B_Low"],
-    "R_High": high_R, "G_High": high_G, "B_High": high_B
-}
-y_pred = self._predict_Y0W_from_models(
-    lut256_iter,
-    panel_text=panel, frame_rate=fr, model_year=model_year
-)
+# ...
+yG[0] = np.nan; yG[255] = np.nan
+out = {"Gamma": yG, "Cx": yCx, "Cy": yCy}
 
-# ★ 디버그 덤프: 현재 반복/컨텍스트 태그로 저장
-self._debug_dump_predicted_Y0W(
-    y_pred,
-    tag=f"iter{it}_{panel}_fr{fr}_my{int(model_year)%100:02d}",
-    save_csv=True
-)
+# (선택) 여기서도 덤프하고 싶으면:
+# self._debug_dump_predicted_Y0W(out, tag="fast_path", save_csv=False)
+
+return out
