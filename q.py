a84835dc1c8c8150ce21444f73f8909c092d7574
Canele-Lemon <gguguggaga@gmail.com>
2025-10-29 10:54:49 +0900

    Update q.py

--- q.py
+++ q.py
@@ -1,10 +1,10 @@
-b1 = self.ax.bar(
-    x - width/2, data_off, width,
-    label=self.series_labels[0],
-    color='gray'
-)
-b2 = self.ax.bar(
-    x + width/2, data_on,  width,
-    label=self.series_labels[1],
-    color='red'
-)
+Traceback (most recent call last):
+  File "d:\LCM_DX\OMS_2\he_opticalmeasurement\subpages\vacspace_130\subpage_vacspace.py", line 1005, in <lambda>
+    self._spec_thread.finished.connect(lambda ok, metrics: self._on_spec_eval_done(ok, metrics, iter_idx=1, max_iters=2))
+  File "d:\LCM_DX\OMS_2\he_opticalmeasurement\subpages\vacspace_130\subpage_vacspace.py", line 1967, in _on_spec_eval_done
+    self._update_spec_views(self._off_store, self._on_store)
+  File "d:\LCM_DX\OMS_2\he_opticalmeasurement\subpages\vacspace_130\subpage_vacspace.py", line 2242, in _update_spec_views
+    duv_off = _delta_uv_for_state(off_store)
+  File "d:\LCM_DX\OMS_2\he_opticalmeasurement\subpages\vacspace_130\subpage_vacspace.py", line 2234, in _delta_uv_for_state
+    _, _, u0, v0 = state_store['colorshift']['main'][idx]  # 정면
+ValueError: not enough values to unpack (expected 4, got 3)
