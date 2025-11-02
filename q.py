1aba324a2860659c458a3e732f5f2990284dd5e3
Canele-Lemon <gguguggaga@gmail.com>
2025-11-02 17:20:42 +0900

    q.py 업데이트

--- q.py
+++ q.py
@@ -1,13 +1,14 @@
-def _is_gray_spec_ok(self, gray:int, *, thr_gamma=0.05, thr_c=0.003) -> bool:
-    ref = self._off_store['gamma']['main']['white'].get(gray, None)
-    on  = self._on_store ['gamma']['main']['white'].get(gray, None)
+def _is_gray_spec_ok(self, gray:int, *, thr_gamma=0.05, thr_c=0.003,
+                     off_store=None, on_store=None) -> bool:
+    off_store = off_store if off_store is not None else self._off_store
+    on_store  = on_store  if on_store  is not None else self._on_store
+    ref = off_store['gamma']['main']['white'].get(gray, None)
+    on  = on_store ['gamma']['main']['white'].get(gray, None)
     if not ref or not on:
         return True
-
     lv_r, cx_r, cy_r = ref
     lv_o, cx_o, cy_o = on
 
-    # Cx/Cy
     dCx = abs(cx_o - cx_r) if (np.isfinite(cx_o) and np.isfinite(cx_r)) else 0.0
     dCy = abs(cy_o - cy_r) if (np.isfinite(cy_o) and np.isfinite(cy_r)) else 0.0
 
@@ -17,6 +18,6 @@ def _is_gray_spec_ok(self, gray:int, *, thr_gamma=0.05, thr_c=0.003) -> bool:
         G_on_g  = self._gamma_from_off_norm_at_gray(self._off_lv_vec, lv_on_g=lv_o, g=gray)
         dG = abs(G_on_g - G_ref_g) if (np.isfinite(G_on_g) and np.isfinite(G_ref_g)) else 0.0
     else:
-        dG = 0.0  # 안전 폴백
+        dG = 0.0
 
     return (dCx <= thr_c) and (dCy <= thr_c) and (dG <= thr_gamma)
